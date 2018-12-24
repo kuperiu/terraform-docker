f9ce8e2347ef02da5ebd12891b5127a74516106f
Lior Kuperiu <liorkuperiu@Liors-MacBook-Pro.local>
2018-12-24 22:09:49 +0100

    [FIX] tag alias from the begining

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
-FROM hashicorp/terraform:0.11.10
+ARG TF_VERSION=0.11.10
+
+FROM hashicorp/terraform:$TF_VERSION
 ENV TF_DATA_DIR /root/.terraform
 RUN apk add bash
 ADD terraform* /root/.terraform/plugins/linux_amd64/
--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+tf_version="0.11.11"
 providers=( "terraform-provider-google" "terraform-provider-google-beta")
 versions=( "1.20.0" "1.20.0" )
 
@@ -8,6 +9,6 @@ for ((i=0;i<${#providers[@]};++i)); do
     unzip ${providers[i]}_${versions[i]}.zip
     rm *.zip
 done
-docker build . -t kuperiu/terraform:0.11.10
-docker push kuperiu/terraform:0.11.10
+docker build --build-arg ${tf_version} . -t kuperiu/terraform:${tf_version} 
+docker push kuperiu/terraform:${tf_version}
 rm -rf terraform*
